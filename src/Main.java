70abc7cc95979224537c15a790392e417abcad25
Tural Guluev <tural-98@bk.ru>
2022-05-31 14:54:45 +0300

    add try/cath

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,19 @@ public class Main {
 
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1, 1);
+        int c=0;
+
+        try {
+            c = calc.devide.apply(a, b);
+
+        } catch (ArithmeticException e) {
+            System.out.println("Деление на ноль!");
+        }
         /*
         ошибка деления на 0, b=0, c=3/0
-        решение поменять первый аргумент метода minus
+        решение добавить блок try/cath
         b=calc.minus.apply(2,1)
          */
-        int c = calc.devide.apply(a, b);
 
         calc.println.accept(c);
     }
